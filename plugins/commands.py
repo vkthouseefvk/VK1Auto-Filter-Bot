a4ea97ae46bbf5f5f50744b1c67a12e8fce5c08e
Hansaka Anuhas <hansakaanuhas154@gmail.com>
2025-05-16 13:05:43 +0530

    done

--- plugins/commands.py
+++ plugins/commands.py
@@ -441,7 +441,7 @@ async def myplan(client, message):
             InlineKeyboardButton('Activate Trial', callback_data='activate_trial'),
             InlineKeyboardButton('Activate Plan', callback_data='activate_plan')
         ]]
-        return await message.reply_photo(random.choice(PICS), 'You dont have any premium plan, please use /plan to activate plan', reply_markup=InlineKeyboardMarkup(btn))
+        return await message.reply_photo(photo=random.choice(PICS), caption='You dont have any premium plan, please use /plan to activate plan', reply_markup=InlineKeyboardMarkup(btn))
     await message.reply(f"You activated {mp['plan']} plan\nExpire: {mp['expire'].strftime('%Y.%m.%d %H:%M:%S')}")
 
 
@@ -452,7 +452,7 @@ async def plan(client, message):
     ],[
         InlineKeyboardButton('Activate Plan', callback_data='activate_plan')
     ]]
-    await message.reply_photo(random.choice(PICS), script.PLAN_TXT.format(PRE_DAY_AMOUNT, RECEIPT_SEND_USERNAME), reply_markup=InlineKeyboardMarkup(btn))
+    await message.reply_photo(photo=random.choice(PICS), caption=script.PLAN_TXT.format(PRE_DAY_AMOUNT, RECEIPT_SEND_USERNAME), reply_markup=InlineKeyboardMarkup(btn))
 
 
 @Client.on_message(filters.command('add_prm') & filters.user(ADMINS))

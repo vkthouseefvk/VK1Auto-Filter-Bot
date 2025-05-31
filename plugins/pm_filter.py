2843af003917fcb1e8f184ddb6d02356e2e18deb
Olakunle <joberror@gmail.com>
2025-05-31 03:02:46 +0100

    refactor: update title callback to delete old message and send a new one to avoid MESSAGE_NOT_MODIFIED error

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1248,16 +1248,16 @@ async def show_title_selection(client, message, titles, s):
 async def title_callback(client, query):
     if query.data.startswith("title#"):
         _, title = query.data.split("#", 1)
+
+        # Instead of editing, delete the old message and send a new one
+        # This completely avoids the MESSAGE_NOT_MODIFIED error
         try:
-            s = await query.message.edit(f"<b><i>🔍 Searching for '{title}'...</i></b>")
-        except Exception as e:
-            # If edit fails, try to send a new message
-            try:
-                await query.message.delete()
-                s = await query.message.reply_text(f"<b><i>🔍 Searching for '{title}'...</i></b>")
-            except:
-                # If all else fails, just send without deleting
-                s = await query.message.reply_text(f"<b><i>🔍 Searching for '{title}'...</i></b>")
+            await query.message.delete()
+        except:
+            pass  # If deletion fails, continue anyway
+
+        # Send a fresh message with the search status
+        s = await query.message.reply_text(f"<b><i>🔍 Searching for '{title}'...</i></b>")
 
         # Pass the actual user ID who clicked the button, not the message sender
         await auto_filter(client, query.message, s, title_search=title, user_id=query.from_user.id)

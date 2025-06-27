65274bcbe523aea015936443c6f3782a54cf7561
vkthouseefvk <131037814+vkthouseefvk@users.noreply.github.com>
2025-06-28 00:28:23 +0530

    Update info.py

--- info.py
+++ info.py
@@ -126,7 +126,7 @@ if len(BIN_CHANNEL) == 0:
     exit()
 else:
     BIN_CHANNEL = int(BIN_CHANNEL)
-URL = environ.get("URL", "")
+URL = environ.get("URL", "-1002839501827")
 if len(URL) == 0:
     logger.error('URL is missing, exiting now')
     exit()
